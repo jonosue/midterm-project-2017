231f743da1f644b15178d0e67ec41c42f7733ba1
Jonathan Sue <hello@jonosue.com>
2017-08-17 19:11:32 +0000

    Changed initial migration file so that our migrations can be implemented properly.

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,9 +1,9 @@
 exports.up = function(knex, Promise) {
   knex.schema.dropTableIfExists('users');
-  return knex.schema.createTable('users', function (table) {
-    table.increments('id');
-    table.string('name');
-  });
+  // return knex.schema.createTable('users', function (table) {
+  //   table.increments('id');
+  //   table.string('name');
+  // });
 };
 
 exports.down = function(knex, Promise) {
